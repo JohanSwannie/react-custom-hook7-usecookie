82b0c5f3e23af731068ca016cad43feebd75285e
jswan <jswannieman@gmail.com>
2023-09-04 16:48:10 +1200

    Update CookieComponent Javascript Component

--- src/CookieComponent.js
+++ src/CookieComponent.js
@@ -1,17 +1,19 @@
 import useCookie from "./useCookie";
 
 const CookieComponent = () => {
-  const [value, deleteTitle, update] = useCookie("title", "Professor");
+  const [value, deleteTitle, updateTitle] = useCookie("title", "Professor");
 
   return (
     <>
       <h3>current title : {value}</h3>
       <div className="buttons">
-        <button onClick={() => update("Doctor")}>Change Title to Doctor</button>
-        <button onClick={() => update("Prime Minister")}>
+        <button onClick={() => updateTitle("Doctor")}>
+          Change Title to Doctor
+        </button>
+        <button onClick={() => updateTitle("Prime Minister")}>
           Change Title to Prime Minister
         </button>
-        <button onClick={() => update("President")}>
+        <button onClick={() => updateTitle("President")}>
           Change Title to President
         </button>
         <button onClick={deleteTitle}>Delete Title</button>
