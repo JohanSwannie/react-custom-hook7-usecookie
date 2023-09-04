c6a26bc18160c16cb33bf00f03013d800228791f
jswan <jswannieman@gmail.com>
2023-09-04 15:25:33 +1200

    Commit CookieComponent Javascript Component

---
+++ src/CookieComponent.js
@@ -0,0 +1,23 @@
+import useCookie from "./useCookie";
+
+const CookieComponent = () => {
+  const [value, update, remove] = useCookie("title", "Professor");
+
+  return (
+    <>
+      <h3>current title : {value}</h3>
+      <div className="buttons">
+        <button onClick={() => update("Doctor")}>Change Title to Doctor</button>
+        <button onClick={() => update("Prime Minister")}>
+          Change Title to Prime Minister
+        </button>
+        <button onClick={() => update("President")}>
+          Change Title to President
+        </button>
+        <button onClick={remove}>Delete Title</button>
+      </div>
+    </>
+  );
+};
+
+export default CookieComponent;
